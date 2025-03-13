d7d8cb6c1b1bfbffce5a316cb10bd499b02ae949
Rodrigo Xavier Vargas <90809287+RodrigoXVargas@users.noreply.github.com>
2025-03-13 09:47:36 -0300

    Update settings.py

--- curso basico/primerProyecto/settings.py
+++ curso basico/primerProyecto/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0grr!c0!9u7h2t(3oc9o#oga7_og*@1qmj@(jx4k8_%v=s#ld@'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
