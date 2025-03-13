98b48f64db8130986d02384dc882a929c47681b8
Rodrigo Xavier Vargas <90809287+RodrigoXVargas@users.noreply.github.com>
2025-03-13 09:43:47 -0300

    Update settings.py
    borro datos sensibles

--- Django ApiRest - simple crud/drfsimplecrud/settings.py
+++ Django ApiRest - simple crud/drfsimplecrud/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%l4g190x&!a-ogsxs7re9^$oqpmo%m2!xr1r0+(f66q1g(x9q)'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
